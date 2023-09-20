192da6b240557de25e5da10b3b7a7038a821cf87
Bill Smith <snowmanam2@gmail.com>
2023-09-19 19:04:52 -0500

    Attempt to reduce postgres SSL disconnects

--- database.py
+++ database.py
@@ -3,6 +3,6 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 SQLALCHEMY_DATABASE_URL = os.environ.get('DB_CONNECTION_STRING')
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
